e248a3d42be4c7ff2b9d6bbd003d11b3a4a31d58
DileepaPRA <prabhath0115@gmail.com>
2026-02-05 13:45:32 +0530

    code base intialization

---
+++ resturarent-system/src/test/java/com/ByteKnights/com/resturarent_system/ResturarentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ByteKnights.com.resturarent_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResturarentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
