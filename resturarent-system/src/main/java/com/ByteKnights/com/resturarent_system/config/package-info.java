1af907839fbbd11a99ca5f9d42987468410cb9ba
DileepaPRA <prabhath0115@gmail.com>
2026-02-14 16:49:55 +0530

    Add dependencies for JWT and configure application properties for database and JPA settings

---
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/config/package-info.java
@@ -0,0 +1,3 @@
+// This package contains global configuration classes.
+// Examples: CorsConfig, WebConfig
+package com.ByteKnights.com.resturarent_system.config;
---
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/controller/package-info.java
@@ -0,0 +1,3 @@
+// This package contains REST controller classes (API endpoints).
+// Examples: AuthController, MenuController, OrderController, UserController
+package com.ByteKnights.com.resturarent_system.controller;
