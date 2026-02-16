2406cb96b0b91557637655c2898e81bb55be2c01
AshenRandira <157471115+AshenRandira@users.noreply.github.com>
2026-02-17 02:08:17 +0530

    done the super admin login api

--- resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/auth/dto/StaffLoginRequest.java
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/auth/dto/StaffLoginRequest.java
@@ -7,6 +7,6 @@ public class StaffLoginRequest {
     public String getEmail() { return email; }
     public String getPassword() { return password; }
 
-    public void setEmail(String email) { this.email = email; }
-    public void setPassword(String password) { this.password = password; }
+    // public void setEmail(String email) { this.email = email; }
+    // public void setPassword(String password) { this.password = password; }
 }
