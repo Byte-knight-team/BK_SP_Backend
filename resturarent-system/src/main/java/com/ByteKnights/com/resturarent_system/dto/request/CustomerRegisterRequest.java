7ecdbc78a61d63b129449f0da3c6c00e81981a2c
DileepaPRA <prabhath0115@gmail.com>
2026-04-18 13:33:44 +0530

    feat: add CustomerRegisterRequest and CustomerRegisterResponseData classes for user registration

---
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/dto/request/CustomerRegisterRequest.java
@@ -0,0 +1,16 @@
+package com.ByteKnights.com.resturarent_system.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CustomerRegisterRequest {
+    private String username;
+    private String email;
+    private String phone;
+    private String password;
+    private String address;
+}
