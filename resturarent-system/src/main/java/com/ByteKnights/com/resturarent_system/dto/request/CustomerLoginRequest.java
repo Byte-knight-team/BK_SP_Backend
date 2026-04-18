e1c8c64c25055b9c2b3e69c1179f1bec25637d1b
DileepaPRA <prabhath0115@gmail.com>
2026-04-18 13:33:54 +0530

    feat: add CustomerLoginRequest and CustomerLoginResponseData classes for user authentication

---
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/dto/request/CustomerLoginRequest.java
@@ -0,0 +1,13 @@
+package com.ByteKnights.com.resturarent_system.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CustomerLoginRequest {
+    private String email;
+    private String password;
+}
