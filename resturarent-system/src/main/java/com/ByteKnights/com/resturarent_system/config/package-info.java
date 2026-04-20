5ace9ddfb989ca766c311f97078b91d6513b6d1d
AshenRandira <157471115+AshenRandira@users.noreply.github.com>
2026-04-20 23:22:59 +0530

    all done in staff invite rbac

--- resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/config/package-info.java
+++
@@ -1,3 +0,0 @@
-// This package contains global configuration classes.
-// Examples: CorsConfig, WebConfig
-package com.ByteKnights.com.resturarent_system.config;
--- resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/dto/package-info.java
+++
@@ -1,3 +0,0 @@
-// This package contains DTO (Data Transfer Object) classes for API requests and responses.
-// Examples: LoginRequest, RegisterRequest, AuthResponse, OrderRequest, OrderResponse
-package com.ByteKnights.com.resturarent_system.dto;
