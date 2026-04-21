4d6046bf41404807ce51259996007dcf326d6fe3
Venuri Perera <pererapavt.23@uom.lk>
2026-04-21 18:33:15 +0530

    feat: add approval and rejection functionality for menu items, including new DTOs and service methods

--- resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/repository/UserRepository.java
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.ByteKnights.com.resturarent_system.repository;
 
 import com.ByteKnights.com.resturarent_system.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,6 +17,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u.id FROM User u JOIN u.role r WHERE UPPER(r.name) LIKE CONCAT('%', UPPER(:roleKeyword), '%')")
+    List<Long> findUserIdsByRoleKeyword(@Param("roleKeyword") String roleKeyword);
 
     // TODO: Add more custom query methods as needed
 }
