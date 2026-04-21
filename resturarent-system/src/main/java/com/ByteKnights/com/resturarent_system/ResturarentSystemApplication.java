c85c43d229415b835f7c05c5e3db9579acfc7965
AshenRandira <157471115+AshenRandira@users.noreply.github.com>
2026-04-21 08:45:19 +0530

    fix: disable default Spring Security user and verify backend startup

--- resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/ResturarentSystemApplication.java
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/ResturarentSystemApplication.java
@@ -2,8 +2,9 @@ package com.ByteKnights.com.resturarent_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class ResturarentSystemApplication {
 
     public static void main(String[] args) {
