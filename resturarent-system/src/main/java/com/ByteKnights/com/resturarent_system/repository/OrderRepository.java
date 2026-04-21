8d6887fa0a28b4900b941cd331791c3596048266
Isuru Adikaram <isadikaram2915@gmail.com>
2026-04-21 14:00:57 +0530

    Implement kitchen most popular meal API

--- resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/repository/OrderRepository.java
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/repository/OrderRepository.java
@@ -4,6 +4,7 @@ import com.ByteKnights.com.resturarent_system.entity.Order;
 import com.ByteKnights.com.resturarent_system.entity.OrderStatus;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -25,4 +26,19 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @EntityGraph(attributePaths = "items")
     Optional<Order> findOrderById(Long id);
+
+
+    // --- Kitchen Queries START ---
+
+    //1.kitchen dashboard stats
+
+    //long countByStatus(OrderStatus status);
+
+    @Query(value = "SELECT AVG(TIMESTAMPDIFF(SECOND, cooking_started_at, cooking_completed_at)) / 60.0 " +
+            "FROM orders WHERE status = 'COMPLETED' AND cooking_started_at IS NOT NULL AND cooking_completed_at IS NOT NULL",
+            nativeQuery = true)
+    Double getAveragePreparationTime();
+
+
+    // --- Kitchen Queries END ---
 }
