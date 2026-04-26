cd97d86dc996c166a56f0112af2b9bc9df000dba
DileepaPRA <prabhath0115@gmail.com>
2026-04-27 00:05:09 +0530

    refactor: remove APPROVED status from OrderStatus enum and update comments for clarity

--- resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/entity/OrderStatus.java
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/entity/OrderStatus.java
@@ -3,12 +3,12 @@ package com.ByteKnights.com.resturarent_system.entity;
 public enum OrderStatus {
     PLACED,
     CANCELLED,
-    APPROVED,
     REJECTED,
     PENDING,
     PREPARING,
     READY,
     OUT_FOR_DELIVERY,
+    ARRIVED, // when delivery arrive targeted location
     SERVED,
     COMPLETED,
     ON_HOLD
@@ -19,15 +19,15 @@ public enum OrderStatus {
  * PREPARING // cheff is preparing the order
  * ON-HOLD // order is on hold - chef hold the order because there is a issue in
  * kitchen side. then receiptionist can take action on it.cancel or update the
+ * receiptionist can too update order state to ON_HOLD
  * order or send back to the kitchen
  * COMPLETED // order is completed - cooking done
- * CLOSED // order is closed - payment done
  * CANCELLED // order is cancelled by receiptionist or user
  */
 
 /*
 PLACED // just placed by customer
-CANCELLED // order is cancelled by user or receiptionist or auto cacelld cause error
+CANCELLED // order is cancelled by user,receiptionist or auto cacelld cause error
 */
 
 /*
