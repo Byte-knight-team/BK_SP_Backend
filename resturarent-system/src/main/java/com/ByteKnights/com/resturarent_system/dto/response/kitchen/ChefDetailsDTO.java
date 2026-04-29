a8c304f18a30eb4f86f74ddb5030d1a3b9856811
Isuru Adikaram <isadikaram2915@gmail.com>
2026-04-29 11:47:38 +0530

    added check out time to the chef details dto

--- resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/dto/response/kitchen/ChefDetailsDTO.java
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/dto/response/kitchen/ChefDetailsDTO.java
@@ -11,6 +11,7 @@ public class ChefDetailsDTO {
     private Long staffId;
     private String fullName;
     private String clockInTime;    // e.g., "08:30 AM" or "Not Checked In"
+    private String clockOutTime;   // e.g., "05:30 PM" or "Not Checked Out"
     private String workStatus;     // e.g., "AVAILABLE", "COOKING", "OFF_DUTY"
     private long totalMealsToday;  // Only meals finished TODAY
 }
