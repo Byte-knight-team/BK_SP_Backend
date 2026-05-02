1242a5405715d5c3601b876e3c56e7e388b11ff1
Isuru Adikaram <isadikaram2915@gmail.com>
2026-05-02 20:24:48 +0530

    updated the dto s by adding relevent annotations

--- resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/dto/request/receptionist/TableActionRequest.java
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/dto/request/receptionist/TableActionRequest.java
@@ -2,11 +2,11 @@ package com.ByteKnights.com.resturarent_system.dto.request.receptionist;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class TableActionRequest {
     @NotNull(message = "Guest count is required")
     @Min(value = 1, message = "At least 1 guest must be seated")
