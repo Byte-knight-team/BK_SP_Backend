aab0768a114f869c885038d3fd8b048e2c525f8e
DileepaPRA <prabhath0115@gmail.com>
2026-06-13 12:39:07 +0530

    feat: implement profile picture upload, update, and removal functionality

--- resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/service/CustomerProfileService.java
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/service/CustomerProfileService.java
@@ -3,9 +3,16 @@ package com.ByteKnights.com.resturarent_system.service;
 import com.ByteKnights.com.resturarent_system.dto.request.customer.CustomerPasswordUpdateRequest;
 import com.ByteKnights.com.resturarent_system.dto.request.customer.CustomerProfileUpdateRequest;
 import com.ByteKnights.com.resturarent_system.dto.response.customer.CustomerProfileResponse;
+import com.ByteKnights.com.resturarent_system.dto.request.customer.ProfilePicturePresignRequest;
+import com.ByteKnights.com.resturarent_system.dto.request.customer.ProfilePictureUpdateRequest;
+import com.ByteKnights.com.resturarent_system.dto.response.customer.ProfilePicturePresignResponse;
 
 public interface CustomerProfileService {
     CustomerProfileResponse getCustomerProfile(String email);
     CustomerProfileResponse updateCustomerProfile(String currentEmail, CustomerProfileUpdateRequest request);
     void updatePassword(String email, CustomerPasswordUpdateRequest request);
+
+    ProfilePicturePresignResponse createProfilePictureUploadUrl(String email, ProfilePicturePresignRequest request);
+    void updateProfilePicture(String email, ProfilePictureUpdateRequest request);
+    void removeProfilePicture(String email);
 }
