2a647049f7f5ec0b8c1780c725a04a76b2d6ba83
Venuri Perera <venuriperera419@gmail.com>
2026-07-14 12:22:49 +0530

    feat: implement WebSocket-based real-time notification service and associated management controllers and services

--- resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/dto/request/admin/MenuItemUpdateDecisionDto.java
+++ resturarent-system/src/main/java/com/ByteKnights/com/resturarent_system/dto/request/admin/MenuItemUpdateDecisionDto.java
@@ -13,4 +13,5 @@ import lombok.NoArgsConstructor;
 public class MenuItemUpdateDecisionDto {
     private MenuItemUpdateRequestStatus status;
     private String adminNote;
+    private String adminName;
 }
